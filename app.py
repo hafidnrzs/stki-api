b3ba24458e25c279eaad9b82a848703e799c4f07
Hafid Nur <hafidnurazis@gmail.com>
2025-01-05 02:06:56 +0700

    build: setup script untuk jalankan deployment di Render.com

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pandas as pd
-from search import search_documents, index_db
+from search import search_documents
 from dotenv import load_dotenv
 import os
 import sqlite3
@@ -12,12 +12,13 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
-# Load dataset
 db_file = os.getenv('DB_FILE')
-conn = sqlite3.connect(db_file)
-news_data = pd.read_sql_query('SELECT * FROM news', conn)
-conn.close()
-index_db(db_file)
+
+def get_news_data():
+    conn = sqlite3.connect(db_file)
+    news_data = pd.read_sql_query('SELECT * FROM news', conn)
+    conn.close()
+    return news_data
 
 @app.route('/news', methods=['GET'])
 def get_news():
@@ -28,6 +29,9 @@ def get_news():
     
     # Get category from query parameters
     category = request.args.get('category', default=None, type=str)
+
+    # Load dataset dynamically
+    news_data = get_news_data()
 
     # Filter news data by category if provided
     if category:
@@ -65,13 +69,14 @@ def search():
 
 @app.route('/news/<int:news_id>', methods=['GET'])
 def get_news_by_id(news_id):
+    news_data = get_news_data()
     news_item = news_data[news_data['id'] == news_id]
     
     if not news_item.empty:
         return jsonify(news_item.iloc[0].to_dict())
     else:
         return jsonify({'error': 'News item not found'}), 404
-    
+
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
